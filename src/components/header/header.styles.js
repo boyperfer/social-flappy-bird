a817dfe48ba99c53d88ac8706317b39a3d75075f
Het Patel <91302274+het1309@users.noreply.github.com>
2024-04-27 21:25:26 -0500

    Update header.styles.js

--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -1,36 +1,35 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
-	background-color: #1c1c1f;
-    width: 100%;
-	height: 100px;
-    display: flex;
-    justify-content: flex-end;
-	color: black;
-	position: fixed;
-	top: 0;
-	right: 0;
-`
-
-export const LinkContainer = styled(Link)`
-    height: 100%;
-    width: 70px;
-    padding: 25px;
-	color: #adb5bd;
-`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 25px;
+  color: black;
+  position: fixed;
+  top: 0;
+  right: 0;
+  border-radius: 10px; /* Add rounded corners */
+`;
 
 export const OptionsContainer = styled.div`
-    width: 50%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-	color: #adb5bd;
-`
+  display: flex; /* Inline elements */
+  align-items: center;
+`;
 
 export const OptionLink = styled(Link)`
-    padding: 10px 15px;
-	color:  #adb5bd;
-`
+  padding: 10px 15px;
+  color: black;
+  text-decoration: none; /* Remove default link underline */
+  font-weight: bold; /* Bold font for emphasis */
+  margin-right: 20px; /* Spacing between links */
+  background-color: #f5f5f5; /* Light background color for options */
+  border-radius: 5px; /* Add rounded corners to options */
+
+  &:hover {
+    color: #333; /* Change color on hover */
+  }
+`;
+
