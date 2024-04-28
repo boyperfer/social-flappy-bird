2428b1df907ea0c533455682ea070f72a41ea4c9
Het Patel <91302274+het1309@users.noreply.github.com>
2024-04-27 21:26:26 -0500

    Update leaderboard.styles.js

--- src/components/leaderboard/leaderboard.styles.js
+++ src/components/leaderboard/leaderboard.styles.js
@@ -1,42 +1,42 @@
 import { styled, css } from "styled-components";
 
 const getBackground = ({ num }) => {
-    console.log(num);
-    return num % 2
-        ? css`
+	console.log(num);
+	return num % 2
+		? css`
               background: #f5f5f5;
           `
-        : css`
+		: css`
               background: white;
           `;
 };
 
 export const LeaderBoardContainer = styled.div`
     width: 40vw;
-    margin: 100px auto;
-	border-collapse: collapse;
-	border-spacing: 0px;
+    margin-left: 0;
+    border-collapse: collapse;
+    border-spacing: 0px;
 `;
 export const RankContainer = styled.div`
-	border-radius: 4px 0px 0px
-	position: relative;
-	height: 32px;
-	user-select: none;
-	background: #282831; 
-	color: #adb5bd;
-	font-size: 14px;
-	line-height: 16px;
-	border-bottom-width: 1px;
-	border-bottom-style: solid;
-	white-space: nowrap;
-	border-color: var(--gray200);
-	box-sizing: border-box;
-	font-weight: bold;
-	vertical-align: middle;
-	padding: 4px;
-	padding-left: 10px;
-	width: 5%;
-`
+    border-radius: 4px 0px 0px;
+    position: relative;
+    height: 32px;
+    user-select: none;
+    background: #282831;
+    color: #adb5bd;
+    font-size: 14px;
+    line-height: 16px;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    white-space: nowrap;
+    border-color: var(--gray200);
+    box-sizing: border-box;
+    font-weight: bold;
+    vertical-align: middle;
+    padding: 4px;
+    padding-left: 10px;
+    width: 5%;
+`;
 export const TierContainer = styled.div`
 	position: relative;
 	height: 32px;
@@ -54,8 +54,7 @@ export const TierContainer = styled.div`
 	vertical-align: middle;
 	padding: 4px;
 	width: 15%;
-`
-
+`;
 export const ScoreContainer = styled.div`
 	position: relative;
 	height: 32px;
@@ -74,28 +73,7 @@ export const ScoreContainer = styled.div`
 	padding: 4px;
 	padding-left: 10px;
 	width: 10%;
-`
-
-export const ExperienceContainer = styled.div`
-	position: relative;
-	height: 32px;
-	user-select: none;
-	background: #282831; 
-	color: #adb5bd; 
-	font-size: 14px;
-	line-height: 16px;
-	border-bottom-width: 1px;
-	border-bottom-style: solid;
-	white-space: nowrap;
-	border-color: var(--gray200);
-	box-sizing: border-box;
-	font-weight: bold;
-	vertical-align: middle;
-	padding: 4px;
-	padding-left: 20px;
-	width: 15%;
-`
-
+`;
 export const DisplayNameContainer = styled.div`
 	position: relative;
 	height: 32px;
@@ -113,14 +91,12 @@ export const DisplayNameContainer = styled.div`
 	vertical-align: middle;
 	padding: 4px;
 	padding-left: 20px;
-	width: 30%;
-`
-
+	width: 70%;
+`;
 export const UsersContainer = styled.div`
     display: flex;
     ${getBackground}
 `;
-
 export const NumberContainer = styled.div`
 	text-align: center;
 	border-bottom-width: 1px;
@@ -143,13 +119,12 @@ export const NumberContainer = styled.div`
 	height: 40px;
 	border-color: black;
 `;
-
-export const NameContainer= styled.div`
+export const NameContainer = styled.div`
 	border-bottom-width: 1px;
 	border-bottom-style: solid;
 	border-color: var(--gray200);
     font-size: 20px;
-    width: 30%;
+    width: 70%;
     font-weight: bold;
     color: #adb5bd;
 	font-size: 12px;
@@ -165,13 +140,12 @@ export const NameContainer= styled.div`
 	height: 40px;
 	border-color: black;
 `;
-
-export const TitleContainer= styled.div`
+export const TitleContainer = styled.div`
 	border-bottom-width: 1px;
 	border-bottom-style: solid;
 	border-color: var(--gray200);
     font-size: 20px;
-    width: 10%;
+    width: 15%;
     font-weight: bold;
     color: #adb5bd;
 	font-size: 12px;
@@ -183,33 +157,9 @@ export const TitleContainer= styled.div`
 	font-family: "Roboto", sans-serif;
 	box-sizing: border-box;
 	padding: 4px;
-	padding-left: 15px;
 	height: 40px;
 	border-color: black;
 `;
-
-export const ExperienceNumberContainer= styled.div`
-	border-bottom-width: 1px;
-	border-bottom-style: solid;
-	border-color: var(--gray200);
-    font-size: 20px;
-    width: 15%;
-    font-weight: bold;
-    color: #adb5bd;
-	font-size: 14px;
-	line-height: 16px;
-	text-align: left !important;
-	font-weight: normal;
-	white-space: nowrap;
-	background: #31313c; 
-	font-family: "Roboto", sans-serif;
-	box-sizing: border-box;
-	padding: 4px;
-	padding-left: 20px;
-	height: 40px;
-	border-color: black;
-
-`
 export const ScoreNumberContainer = styled.div`
 	border-bottom-width: 1px;
 	border-bottom-style: solid;
@@ -227,8 +177,7 @@ export const ScoreNumberContainer = styled.div`
 	font-family: "Roboto", sans-serif;
 	box-sizing: border-box;
 	padding: 4px;
-	padding-left: 20px;
+	padding-left: 30px;
 	height: 40px;
 	border-color: black;
-
-`
+`;
