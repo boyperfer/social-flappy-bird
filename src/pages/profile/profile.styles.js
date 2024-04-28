3103fd293ddae1c5976cf8b8857a7e45bfe0335c
Het Patel <91302274+het1309@users.noreply.github.com>
2024-04-27 21:35:01 -0500

    Update profile.styles.js

--- src/pages/profile/profile.styles.js
+++ src/pages/profile/profile.styles.js
@@ -7,7 +7,14 @@ export const ProfileContainer = styled.div`
     justify-content: center;
 `;
 
+export const ProfileInfo = styled.div`
+    margin: 8px auto;
+    padding: 20px;
+    font-size: 30px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Shadow effect */
+`;
+
 export const TextContainer = styled.div`
     margin: 8px auto;
-    font-size: 30px;
+    font-size: 50px;
 `;
