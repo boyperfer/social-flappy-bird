04eee306dedaf11feb724695ff2231184d62ecea
Jeongsik Park <69391959+JeongSikPark1998@users.noreply.github.com>
2024-04-29 23:06:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # login
-echo "Install conda environment"
-conda create -n social-flappy-bird python=3.11.2
-conda activate social-flappy-bird
+# echo "Install conda environment"
+# conda create -n social-flappy-bird python=3.11.2
+# conda activate social-flappy-bird
 
 echo "Install python packages"
 cd soical-flappy-bird
